35c9d79a57561e83b1d4d8698323fc88ee64390d
mahedi1318 <mahedi.mern@gmail.com>
2024-02-05 18:19:26 +0600

    task List all data  dainamik commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,9 @@
 const Footer = () => {
   return (
     <div>
-      <footer class="py-6 md:py-8">
-		<div class="container mx-auto">
-			<p class="text-center text-xs text-[#6A6A6A] lg:text-sm">
+      <footer className="py-6 md:py-8">
+		<div className="container mx-auto">
+			<p className="text-center text-xs text-[#6A6A6A] lg:text-sm">
 				Copyright ©2024 | All rights reserved by Learn with Sumit
 			</p>
 		</div>
--- src/components/TaskBoard.jsx
+++ src/components/TaskBoard.jsx
@@ -1,22 +1,35 @@
+import { useState } from "react"
 import Search from "./Tasks/Search"
 import TaskAction from "./Tasks/TaskAction"
 import TaskList from "./Tasks/TaskList"
 
 
 const TaskBoard = () => {
+
+	let taskDefultData = {
+		id : crypto.randomUUID(),
+		title: "Integration API",
+		description: "Connect an existing API to a third-party database using secure methods and handle data exchange efficiently.",
+		tags: ['Web', 'Python', 'API'] ,
+		priority : "High",
+		isFavorite : "false"
+	}
+
+	let [tasks, setTasks] = useState([taskDefultData]);
+
   return (
     <>
-      <section class="mb-20 flex flex-col justify-center items-center" id="tasks">
+      <section className="mb-20 flex flex-col justify-center items-center" id="tasks">
 		
-		<div class="container">
+		<div className="container">
 		
-		<div class="p-2 flex justify-end">
+		<div className="p-2 flex justify-end">
 			<Search/>
 		</div>
 		
-			<div class="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
+			<div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
 				<TaskAction/>
-				<TaskList/>
+				<TaskList tasks={tasks}/>
 			</div>
 		</div>
 	</section>
